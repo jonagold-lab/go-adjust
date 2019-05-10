105f47062d9693ee869fee41f13d3e440950517d
Stefan Maier <stefan.maier09@gmail.com>
2019-05-10 10:44:45 +0400

    Table test KPI List (#1)
    * init try for table tests
    
    * fixed requirements

--- adjust/kpis_test.go
+++ adjust/kpis_test.go
@@ -2,65 +2,64 @@ package adjust
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"reflect"
 	"testing"
 )
 
-func TestKPIService_List(t *testing.T) {
-	client, mux, _, teardown := setup()
-	t.Log("Setup Done")
-	defer teardown()
-
-	mux.HandleFunc("/kpis/v1/random.json", func(w http.ResponseWriter, r *http.Request) {
-		testMethod(t, r, "GET")
-		w.WriteHeader(http.StatusOK)
-		w.Write(loadFixture("kpis.json"))
-	})
-	opt := Options{}
-	got, _, err := client.KPI.List(context.Background(), &opt)
-	if err != nil {
-		t.Errorf("KPI.List returned error: %v", err)
-	}
-
-	want := &KPI{}
-	responseToInterface(loadFixture("kpis.json"), &want)
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("KPI.List = %+v, want %+v", got, want)
-	}
+var testCases = []struct {
+	name    string
+	path    string
+	fixture string
+	opts    *Options
+}{
+	{
+		"No Options",
+		"/kpis/v1/random.json",
+		"kpis.json",
+		nil,
+	},
+	{
+		"With Options",
+		"/kpis/v1/random.json",
+		"kpis_options.json",
+		&Options{
+			AttributionSource: "dynamic",
+			AttributionType:   "click",
+			StartDate:         "2019-05-01",
+			EndDate:           "2019-05-09",
+			UTCOffset:         "00:00",
+			EventKpis:         "tam0sc_events,xxwrmf_events,wk9hmc_events,tucsbx_events",
+			Kpis:              "installs",
+			Sandbox:           false,
+			HumanReadableKpis: true,
+			Grouping:          "day,networks,campaigns,adgroups,creatives",
+			Reattributed:      "all",
+		},
+	},
 }
 
-func TestKPIService_ListWithOptions(t *testing.T) {
-	client, mux, _, teardown := setup()
-	t.Log("Setup Done")
-	defer teardown()
-
-	mux.HandleFunc("/kpis/v1/random.json", func(w http.ResponseWriter, r *http.Request) {
-		testMethod(t, r, "GET")
-		w.WriteHeader(http.StatusOK)
-		w.Write(loadFixture("kpis_options.json"))
-	})
-	opt := Options{
-		AttributionSource: "dynamic",
-		AttributionType:   "click",
-		StartDate:         "2019-05-01",
-		EndDate:           "2019-05-09",
-		UTCOffset:         "00:00",
-		EventKpis:         "tam0sc_events,xxwrmf_events,wk9hmc_events,tucsbx_events",
-		Kpis:              "installs",
-		Sandbox:           false,
-		HumanReadableKpis: true,
-		Grouping:          "day,networks,campaigns,adgroups,creatives",
-		Reattributed:      "all",
-	}
-	got, _, err := client.KPI.List(context.Background(), &opt)
-	if err != nil {
-		t.Errorf("KPI.List returned error: %v", err)
-	}
-
-	want := &KPI{}
-	responseToInterface(loadFixture("kpis_options.json"), &want)
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("KPI.List = %+v, want %+v", got, want)
+func TestKPIService_List(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			client, mux, _, teardown := setup()
+			t.Log(fmt.Sprintf("Setup Done %s", tc.name))
+			defer teardown()
+			mux.HandleFunc(tc.path, func(w http.ResponseWriter, r *http.Request) {
+				testMethod(t, r, "GET")
+				w.WriteHeader(http.StatusOK)
+				w.Write(loadFixture(tc.fixture))
+			})
+			got, _, err := client.KPI.List(context.Background(), tc.opts)
+			if err != nil {
+				t.Errorf("TestKPIService - %s : KPI.List returned error: %v", tc.name, err)
+			}
+			want := &KPI{}
+			responseToInterface(loadFixture(tc.fixture), &want)
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("TestKPIService - %s : KPI.List = %+v, want %+v", tc.name, got, want)
+			}
+		})
 	}
 }
